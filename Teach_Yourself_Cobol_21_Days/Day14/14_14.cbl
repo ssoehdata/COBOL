3bf8d0029eeb3aa30a1b787302a5078929480f48
S <ssoehnlen@protonmail.com>
2025-05-27 22:50:12 +0200

    finishing week2

---
+++ Teach_Yourself_Cobol_21_Days/Day14/14_14.cbl
@@ -0,0 +1,14 @@
+      * MULTIPLE IF ELSE statements
+       DO-THE-PICK.
+           IF MENU-PICK = 1 
+               PERFORM ADD-MODE 
+           ELSE 
+           IF MENU-PICK = 2 
+               PERFORM CHANGE-MODE 
+           ELSE 
+           IF MENU-PICK = 3 
+               PERFORM INQUIRE-MODE 
+           ELSE 
+           IF MENU-PICK = 4 
+               PERFORM DELETE-MODE. 
+       
---
+++ Teach_Yourself_Cobol_21_Days/Day14/14_15.cbl
@@ -0,0 +1,15 @@
+      * Indented IF ELSE statements 
+       DO-THE-PICK.
+           IF MENU-PICK = 1 
+               PERFORM ADD-RECORDS 
+           ELSE 
+               IF MENU-PICK = 2 
+                   PERFORM CHANGE-RECORDS 
+               ELSE 
+                   IF MENU-PICK = 3 
+                       PERFORM LOOK-UP-RECORDS 
+                   ELSE 
+                       IF MENU-PICK = 4 
+                           PERFORM DELETE-RECORDS.
+
+       
