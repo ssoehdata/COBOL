61c7bc33b31d0a448cf400a3b99caf7be48c9d4a
S <ssoehnlen@protonmail.com>
2025-06-21 07:17:20 +0200

    adding first batch of bonus days

---
+++ Teach_Yourself_Cobol_21_Days/Bonus_Days/b1_1.cbl
@@ -0,0 +1,10 @@
+      * Bonus Day 1
+      * Control Break Variables 
+      *---------------------------
+      * The total for  control breaks 
+      *--------------------------------
+       77  ABC-TOTAL               PIC S9(6)V99. 
+       77  PHONE-TOTAL             PIC S9(6)V99. 
+       77  MOON-TOTAL              PIC S9(6)V99. 
+       77  GRAND-TOTAL             PIC S9(6)V99. 
+       
